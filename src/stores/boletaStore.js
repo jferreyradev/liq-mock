ff224586d734b21f4a914526c774c497230e6dc4
José R. Ferreyra <jferreyra.dev@gmail.com>
2024-03-19 00:45:35 -0300

    boleta doble

--- src/stores/boletaStore.js
+++ src/stores/boletaStore.js
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia"
-import { computed, ref } from "vue"
+import { ref } from "vue"
 import { PDFDocument, StandardFonts, rgb } from 'pdf-lib'
 import download from 'downloadjs'
 import logo from '../assets/logohcdt1.png'
