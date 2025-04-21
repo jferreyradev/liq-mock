7daeb61666c1bd97f70172902e18eeee0a8664e1
José R. Ferreyra <jferreyra.dev@gmail.com>
2025-04-20 23:25:10 -0300

    listo para merge

--- src/components/Novedades/llamadaAPI.js
+++ src/components/Novedades/llamadaAPI.js
@@ -5,9 +5,8 @@ import { useEndPoints } from '@/composables/useEndPoints'
 const { apiBase } = useEndPoints()
 
 //const urlAPI = 'http://www.serverburru2.duckdns.org:3005/api/'
-//const urlAPI = apiBase.value + '/api/'
-const urlAPI = 'http://181.87.3.66:3005/api/'
 
+const urlAPI = apiBase.value + '/api/'
 const urlAPI_sp = 'https://josrferreyr-deno-api-su-79.deno.dev/'
 
 export async function grabarRegistro(url = '', data = {}, metodo = 'POST') {
