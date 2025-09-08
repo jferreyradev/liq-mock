e2caf97d1ef2efa9b1cdbe6200b59773f328724a
José R. Ferreyra <jferreyra.dev@gmail.com>
2025-09-08 11:00:11 -0300

    prueba burru

--- src/router/index.js
+++ src/router/index.js
@@ -114,7 +114,15 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AcreditacionesView.vue')
-    }
+    },{
+      path: '/procesos',
+      name: 'procesos',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+       component: () => import('../views/ProcesosView.vue')
+    },
+
   ]
 })
 
